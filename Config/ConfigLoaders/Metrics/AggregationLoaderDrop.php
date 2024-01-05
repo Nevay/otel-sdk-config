8ea3a131607cda9a653506b7bfd32b3327b99c97
Tobias Bachert <git@b-privat.de>
2024-01-05 15:40:57 +0100

    Add drop aggregation

--- Config/ConfigLoaders/Metrics/AggregationLoaderDrop.php
+++ Config/ConfigLoaders/Metrics/AggregationLoaderDrop.php
@@ -4,12 +4,13 @@ namespace Nevay\OtelSDK\Configuration\Config\ConfigLoaders\Metrics;
 use Nevay\OtelSDK\Configuration\Config\Loader;
 use Nevay\OtelSDK\Configuration\Config\LoaderRegistry;
 use Nevay\OtelSDK\Configuration\Context;
+use Nevay\OtelSDK\Metrics\Aggregation\DropAggregationResolver;
 use Nevay\OtelSDK\Metrics\AggregationResolver;
 
 final class AggregationLoaderDrop implements Loader {
 
-    public function load(array $config, LoaderRegistry $registry, Context $context): AggregationResolver|false {
-        return false;
+    public function load(array $config, LoaderRegistry $registry, Context $context): AggregationResolver {
+        return new DropAggregationResolver();
     }
 
     public function type(): string {
