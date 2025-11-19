dffdabfb1d4af3918dc0501de9044a5ec7b130e6
Tobias Bachert <git@b-privat.de>
2025-11-19 14:52:06 +0100

    Add `temporality` and `default_histogram_aggregation` to console metric exporter

--- Config/Metrics/MetricExporterConsole.php
+++ Config/Metrics/MetricExporterConsole.php
@@ -1,8 +1,13 @@
 <?php declare(strict_types=1);
 namespace Nevay\OTelSDK\Configuration\Config\Metrics;
 
+use Nevay\OTelSDK\Metrics\Aggregation\Base2ExponentialBucketHistogramAggregation;
+use Nevay\OTelSDK\Metrics\Aggregation\DefaultAggregation;
+use Nevay\OTelSDK\Metrics\Aggregation\ExplicitBucketHistogramAggregation;
+use Nevay\OTelSDK\Metrics\InstrumentType;
 use Nevay\OTelSDK\Metrics\MetricExporter;
 use Nevay\OTelSDK\Otlp\OtlpStreamMetricExporter;
+use Nevay\OTelSDK\Otlp\OtlpTemporality;
 use Nevay\SPI\ServiceProviderDependency\PackageDependency;
 use OpenTelemetry\API\Configuration\Config\ComponentProvider;
 use OpenTelemetry\API\Configuration\Config\ComponentProviderRegistry;
@@ -19,17 +24,43 @@ use function Amp\ByteStream\getStdout;
 final class MetricExporterConsole implements ComponentProvider {
 
     /**
-     * @param array{} $properties
+     * @param array{
+     *     temporality_preference: 'cumulative'|'delta'|'lowmemory',
+     *     default_histogram_aggregation: 'explicit_bucket_histogram'|'base2_exponential_bucket_histogram',
+     * } $properties
      */
     public function createPlugin(array $properties, Context $context): MetricExporter {
         return new OtlpStreamMetricExporter(
             stream: getStdout(),
+            temporalityResolver: match ($properties['temporality_preference']) {
+                'cumulative' => OtlpTemporality::Cumulative,
+                'delta' => OtlpTemporality::Delta,
+                'lowmemory' => OtlpTemporality::LowMemory,
+            },
+            aggregation: (new DefaultAggregation())->with(InstrumentType::Histogram, match ($properties['default_histogram_aggregation']) {
+                'explicit_bucket_histogram' => new ExplicitBucketHistogramAggregation(),
+                'base2_exponential_bucket_histogram' => new Base2ExponentialBucketHistogramAggregation(),
+            }),
             meterProvider: $context->meterProvider,
             logger: $context->logger,
         );
     }
 
     public function getConfig(ComponentProviderRegistry $registry, NodeBuilder $builder): ArrayNodeDefinition {
-        return $builder->arrayNode('console');
+        $node = $builder->arrayNode('console');
+        $node
+            ->children()
+                ->enumNode('temporality_preference')
+                    ->values(['cumulative', 'delta', 'lowmemory'])
+                    ->defaultValue('cumulative')
+                ->end()
+                ->enumNode('default_histogram_aggregation')
+                    ->values(['explicit_bucket_histogram', 'base2_exponential_bucket_histogram'])
+                    ->defaultValue('explicit_bucket_histogram')
+                ->end()
+            ->end()
+        ;
+
+        return $node;
     }
 }
